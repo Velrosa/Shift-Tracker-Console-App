36aea5d93adad86322a9c047e120493dbe13dbef
Velrosa <steezie21@hotmail.co.uk>
2022-04-12 16:30:48 +0100

    Add project files.

---
+++ Shift Tracker Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shift_Tracker_Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
